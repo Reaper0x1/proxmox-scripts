cfe56835a9b4bdcd037fe8bea8a030686784a81d
Reaper0x1 <reaper0x1@gmail.com>
2024-06-08 02:18:56 +0200

    Fix and improvements

--- wazuh/configure-docker-listener.sh
+++ wazuh/configure-docker-listener.sh
@@ -7,7 +7,7 @@ for CONTAINER in ${CONTAINERS[@]}
 do
     has_agent=$(pct exec $CONTAINER -- test -d /var/ossec && echo "yes" || echo "no")
     has_docker=$(pct exec $CONTAINER -- docker --version &> /dev/null && echo "yes" || echo "no")
-    if [[ $has_agent == "yes" && $CONTAINER != $WAZUH_MANAGER_CONTAINER && $has_docker ]]; then
+    if [[ $has_agent == "yes" && $CONTAINER != $WAZUH_MANAGER_CONTAINER && $has_docker == "yes" ]]; then
 
         echo "# Configuring $CONTAINER -----------------------------------------------------------------------------------"
         echo "# Has docker: $has_docker"
@@ -24,16 +24,26 @@ do
 
         if [[ $has_docker_module == "no" ]]; then
             echo "# [$CONTAINER] - Installing python docker module..."
-            pct exec $CONTAINER -- pip3 install docker==4.2.0
+            pct exec $CONTAINER -- pip3 install docker==4.2.0 --break-system-packages
         else
             echo "# [$CONTAINER] - Python docker module already installed"
             sleep 1
         fi
 
         echo "# [$CONTAINER] - Enabling the Wazuh agent to receive remote commands from the Wazuh server..."
-        echo "echo 'logcollector.remote_commands=1' >> /var/ossec/etc/local_internal_options.conf" | pct enter 100
+
+        remote_command_enabled=$(pct exec $CONTAINER -- cat /var/ossec/etc/local_internal_options.conf 2>/dev/null | grep logcollector.remote_commands >/dev/null && echo "yes" || echo "no>        echo "Has remote command enabled: $remote_command_enabled"
+
+        if [[ $remote_command_enabled == "no" ]]; then
+            echo "echo 'logcollector.remote_commands=1' >> /var/ossec/etc/local_internal_options.conf" | pct enter $CONTAINER
+        else
+            echo "# [$CONTAINER] - Remote command already enabled"
+            sleep 1
+        fi
 
         echo "# [$CONTAINER] - Restarting the agent..."
         pct exec $CONTAINER -- systemctl restart wazuh-agent
     fi
 done
+
+
