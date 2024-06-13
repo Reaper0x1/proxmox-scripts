b2ec914efa511d0c4140bbc5a4f2c8852161d6fa
Reaper0x1 <reaper0x1@gmail.com>
2024-06-13 04:59:42 +0200

    Fix

--- crowdsec/configure-discord.sh
+++ crowdsec/configure-discord.sh
@@ -3,6 +3,6 @@
 if [[ -d /etc/crowdsec  ]]; then
     echo "# Configuring Discord notifications on container $CONTAINER -----------------------------------------------------------------------------------"
     wget https://raw.githubusercontent.com/Reaper0x1/proxmox-scripts/main/crowdsec/discord.yaml -O /etc/crowdsec/notifications/discord.yaml
-    cat /etc/crowdsec/profiles.yaml  | sed '/on_success:/i notifications:\n - discord' | tee /etc/crowdsec/profiles.yaml > /dev/null
+    sed '/on_success:/i notifications:\n - discord' /etc/crowdsec/profiles.yaml > /tmp/profiles.yaml && mv /tmp/profiles.yaml /etc/crowdsec/profiles.yaml
     systemctl restart crowdsec
 fi
