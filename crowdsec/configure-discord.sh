516e5bcb4f1327ab44d699ee7b193e95af735537
Reaper0x1 <reaper0x1@gmail.com>
2024-06-13 04:31:23 +0200

    Update crowdsec

--- crowdsec/configure-discord.sh
+++ crowdsec/configure-discord.sh
@@ -4,13 +4,11 @@ CONTAINERS=( $(pct list | grep running | awk '{print $1}') )
 
 for CONTAINER in ${CONTAINERS[@]}
 do
-    CONTAINER=102
-    if [[ -d /etc/crowdsec ]]; then
+    has_crowdsec=$(pct exec $CONTAINER -- test -d /etc/crowdsec && echo "yes" || echo "no")
+    if [[ $has_crowdsec == "yes"  ]]; then
         echo "# Configuring Discord notifications on container $CONTAINER -----------------------------------------------------------------------------------"
-        pct push ./discord.yaml /etc/crowdsec/notifications/
+        pct push $CONTAINER ./discord.yaml /etc/crowdsec/notifications/discord.yaml
         pct exec $CONTAINER -- bash -c "cat /etc/crowdsec/profiles.yaml  | sed '/on_success:/i notifications:\n - discord' > /etc/crowdsec/profiles.yaml"
         pct exec $CONTAINER -- systemctl restart crowdsec
     fi
 done
-
-
