1922ee5b4ff7018ea381e2de31bc8530a82e54f9
Reaper0x1 <reaper0x1@gmail.com>
2024-06-13 05:09:14 +0200

    Fix

--- crowdsec/configure-discord.sh
+++ crowdsec/configure-discord.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
 if [[ -d /etc/crowdsec  ]]; then
-    echo "# Configuring Discord notifications on container $CONTAINER -----------------------------------------------------------------------------------"
+    echo "# Configuring Discord notifications-----------------------------------------------------------------------------------"
+
+    printf "# Enter your Discord Webhook URL: "
+    read -r discord_webhook
+
     wget https://raw.githubusercontent.com/Reaper0x1/proxmox-scripts/main/crowdsec/discord.yaml -O /etc/crowdsec/notifications/discord.yaml
+    sed -i "s/<discord-webhook>/$discord_webhook/g" /etc/crowdsec/notifications/discord.yaml
+
     sed '/on_success:/i notifications:\n - discord' /etc/crowdsec/profiles.yaml > /tmp/profiles.yaml && mv /tmp/profiles.yaml /etc/crowdsec/profiles.yaml
     systemctl restart crowdsec
 fi
