c3d8392cd7d0811ceb83e0c160f14adef820165e
Reaper0x1 <reaper0x1@gmail.com>
2024-06-13 05:11:19 +0200

    Fix escaping

--- crowdsec/configure-discord.sh
+++ crowdsec/configure-discord.sh
@@ -7,7 +7,8 @@ if [[ -d /etc/crowdsec  ]]; then
     read -r discord_webhook
 
     wget https://raw.githubusercontent.com/Reaper0x1/proxmox-scripts/main/crowdsec/discord.yaml -O /etc/crowdsec/notifications/discord.yaml
-    sed -i "s/<discord-webhook>/$discord_webhook/g" /etc/crowdsec/notifications/discord.yaml
+    escaped_webhook=$(printf '%q' "$discord_webhook")
+    sed -i "s/<discord-webhook>/'$escaped_webhook'/g" /etc/crowdsec/notifications/discord.yaml
 
     sed '/on_success:/i notifications:\n - discord' /etc/crowdsec/profiles.yaml > /tmp/profiles.yaml && mv /tmp/profiles.yaml /etc/crowdsec/profiles.yaml
     systemctl restart crowdsec
