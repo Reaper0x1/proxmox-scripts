d95d1a09a3f1a7cfe9611e2154d3b2ea1ef486ed
Reaper0x1 <reaper0x1@gmail.com>
2024-06-13 05:19:52 +0200

    Fix

--- crowdsec/configure-discord.sh
+++ crowdsec/configure-discord.sh
@@ -8,7 +8,7 @@ if [[ -d /etc/crowdsec  ]]; then
 
     wget https://raw.githubusercontent.com/Reaper0x1/proxmox-scripts/main/crowdsec/discord.yaml -O /etc/crowdsec/notifications/discord.yaml
     escaped_webhook=$(echo "$discord_webhook" | sed 's/[&/\]/\\&/g')
-    sed "s/<discord-webhook>/$escaped_webhook/g" /etc/crowdsec/notifications/discord.yaml > /tmp/discord.yaml && mv /tmp/discord.yaml /etc/crowdsec/notifications/discord.yaml
+    sed -e 's/<discord-webhook>/$escaped_webhook/g' /etc/crowdsec/notifications/discord.yaml > /tmp/discord.yaml && mv /tmp/discord.yaml /etc/crowdsec/notifications/discord.yaml
 
     sed '/on_success:/i notifications:\n - discord' /etc/crowdsec/profiles.yaml > /tmp/profiles.yaml && mv /tmp/profiles.yaml /etc/crowdsec/profiles.yaml
     systemctl restart crowdsec
